fc3989f3a8967c633639745559c29f3853082d8c
Anwar <anwar-360@hotmail.com>
2020-03-31 09:21:51 +0200

    added config

--- config/test.exs
+++ config/test.exs
@@ -2,11 +2,12 @@ use Mix.Config
 
 # Configure your database
 config :fniks_db, FniksDb.Repo,
-  username: "root",
-  password: "",
-  database: "fniks_db_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+username: System.get_env("TEST_DB_USER") || "root",
+  password: System.get_env("TEST_DB_PASSWORD") || "t",
+  database: System.get_env("TEST_DB_NAME") || "fniks_db_test",
+  hostname: System.get_env("TEST_DB_HOST") || "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: (System.get_env("DB_POOL_SIZE") || "10") |> Integer.parse() |> elem(0)
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
